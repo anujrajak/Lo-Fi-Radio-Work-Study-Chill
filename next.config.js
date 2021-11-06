da9cf5d46f2777447003faff9fb22c28c96aadd0
Anuj Rajak <mr.anujrajak@gmail.com>
2021-11-06 05:58:41 +0530

    removing workflow

--- next.config.js
+++ next.config.js
@@ -5,6 +5,5 @@ module.exports = withPWA({
     dest: "public",
     register: true,
     skipWaiting: true,
-  },
-  assetPrefix: 'https://github.com/anujrajak/Lo-Fi-Radio-Work-Study-Chill/blob/gh-pages/'
+  }
 });
